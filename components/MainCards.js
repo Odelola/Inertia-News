e82c5957f2d49d49afbe90bc70727962d3c5f6a1
Odelola <integralandintegrations@gmail.com>
2022-07-25 12:26:38 +0100

    Changed UI and suspended error

--- components/MainCards.js
+++ components/MainCards.js
@@ -10,7 +10,7 @@ const MainCards = ({ data, id }) => {
           <img src={data.urlToImage} alt={data.description} className="w-28 h-28   object-cover rounded-xl bg-center" />
         </div>
         <div className="news-card_desc">
-          <Link href={`/news/${id}`}>
+          <Link href={``}>
             <h3 className="news-card_heading font-nunito font-bold text-[17px]font-nunito text-[#170F49] hover:underline">{data.title}</h3>
           </Link>
           {/* <p className="news-card_text font-zilla text-[#514F6E]">{`${data.content.substring(0, 99)} [...]`} <a href="#0">Read More</a></p> */}
