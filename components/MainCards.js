3a1f8514677930dc2c7751094a5f967268909aa7
Odelola <integralandintegrations@gmail.com>
2022-07-25 12:30:37 +0100

    Changed UI and second suspended error

--- components/MainCards.js
+++ components/MainCards.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 
-const MainCards = ({ data, id }) => {
+const MainCards = ({ data }) => {
 
     return (
       <div className="news-card p-4 flex flex-row-reverse justify-between max-w-[700px] border border-[#E0E1EC] shadow-sm rounded-lg">
@@ -10,7 +10,7 @@ const MainCards = ({ data, id }) => {
           <img src={data.urlToImage} alt={data.description} className="w-28 h-28   object-cover rounded-xl bg-center" />
         </div>
         <div className="news-card_desc">
-          <Link href={``}>
+          <Link href={`#0`}>
             <h3 className="news-card_heading font-nunito font-bold text-[17px]font-nunito text-[#170F49] hover:underline">{data.title}</h3>
           </Link>
           {/* <p className="news-card_text font-zilla text-[#514F6E]">{`${data.content.substring(0, 99)} [...]`} <a href="#0">Read More</a></p> */}
